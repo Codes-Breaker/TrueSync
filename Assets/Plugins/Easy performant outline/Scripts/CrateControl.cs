9d65f5a05937632f01a535f69787e1cc9d2b28f5
linmohan <466343622@qq.com>
2023-04-18 18:03:05 +0800

    道具外描边

---
+++ Assets/Plugins/Easy performant outline/Scripts/CrateControl.cs
@@ -0,0 +1,25 @@
+﻿using EPOOutline;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public List<Outlinable> outlinables;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(var outline in outlinables)
+        {
+            var currentCoef = Mathf.Abs(Mathf.Sin(Time.time));
+            outline.OutlineParameters.DilateShift = currentCoef;
+        }
+    }
+}
