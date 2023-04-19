ed6c2588d019e4ac5f35e5f0f40a35f46c8b7254
Harry <liyawen117967@pwrd.com>
2023-04-19 17:02:27 +0800

    撞击特效

--- Assets/Script/SkillItem/SkillItemControllerBase.cs
+++ Assets/Script/SkillItem/SkillItemControllerBase.cs
@@ -76,7 +76,7 @@ public class SkillItemControllerBase : MonoBehaviour
     {
         user.canvas.transform.forward = Camera.main.transform.forward;
         icon.transform.position = user.transform.position;
-        icon.transform.localPosition = icon.transform.localPosition + new Vector3(0, 2.1f + (user.bodyCollider.transform.localScale.x - 1) * 1.2f, 0);
+        icon.transform.localPosition = icon.transform.localPosition + new Vector3(0.5f, 2.1f + (user.bodyCollider.transform.localScale.x - 1) * 1.2f, 0);
     }
 
     public virtual void ExitUseMode()
