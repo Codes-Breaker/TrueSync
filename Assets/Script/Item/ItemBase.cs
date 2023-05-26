9d905e4e2d28a87885030552f9ab4d01ca34506a
linmohan <466343622@qq.com>
2023-05-26 15:49:14 +0800

    空中撞击力重新计算 加 道具基类

--- Assets/Script/Item/ItemBase.cs
+++ Assets/Script/Item/ItemBase.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ItemBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("µÀ¾ßµÄGameObject")]
+    public GameObject itemGameObject;
+    private float stayTime;
+    private float currentTime;
+    private bool isShow = false;
+    public ItemBuffBase itemBuffBase;
+
+    public virtual void CreatSkillItemm(float stayTimeData)
     {
-        
+        stayTime = stayTimeData;
+        isShow = true;
+        itemGameObject.SetActive(true);
+        itemGameObject.transform.DOLocalRotate(new Vector3(0, 360, 0), 1f, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Restart)
+            .SetEase(Ease.Linear);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.GetComponent<CharacterContorl>())
+        {
+            var otherCollision = collision.gameObject.GetComponent<CharacterContorl>();
+            otherCollision.buffs.Add(CreatItemBuff());
+        }
+    }
+
+    protected virtual ItemBuffBase CreatItemBuff()
+    {
+        return null;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!isShow)
+            return;
+        currentTime += Time.deltaTime;
+        if (currentTime > stayTime)
+        {
+            OnEnd();
+        }
+
+    }
+
+    public virtual void OnEnd()
+    {
+        DOTween.Kill(itemGameObject.transform);
+        GameObject.Destroy(gameObject);
     }
 }
