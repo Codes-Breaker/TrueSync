3a351cb70c404e74dad896801c4c7bf49106cf4c
Harry <liyawen117967@pwrd.com>
2023-06-19 15:29:12 +0800

    扔

--- Assets/Script/Item/ItemAbility/ProjectileItemAbility.cs
+++ Assets/Script/Item/ItemAbility/ProjectileItemAbility.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class ProjectileItemAbility : ItemAbilityBase
 {
     protected float offset = 1f;
+    private ItemProjectileBase itemBase;
     public ProjectileItemAbility(CharacterContorl character, ItemData data) : base(character, data)
     {
+        character.animationEventReceiver.RegisterEvent(AnimationEventReceiver.EventEnum.ThrowBoom, Launch);
     }
 
     public override void Init()
@@ -29,9 +31,20 @@ public class ProjectileItemAbility : ItemAbilityBase
         var point = (character.transform.position + (character.bodyCollider as SphereCollider).center) + (character.ridbody.transform.forward.normalized * (character.bodyCollider as SphereCollider).radius * character.transform.localScale.x + character.ridbody.transform.forward.normalized * offset);
         //trapObject.transform.position = new Vector3(point.x, point.y - (character.bodyCollider as SphereCollider).radius * character.transform.localScale.x, point.z);
         //trapObject.transform.position = point;
-        ItemManager.CreatProjectileByItemID(itemData.itemId, character,character.ridbody.transform.forward,point);
-
+        itemBase = ItemManager.CreatProjectileByItemID(itemData.itemId, character, point);
+        itemBase.Throw();
+        Throw();
         base.itemAbility();
+    }
+
+    private void Throw()
+    {
+        character.anima.SetTrigger("throwBoom");
+    }
+
+    private void Launch()
+    {
+        itemBase.Launch();
     }
 
     public override void FixedUpdate()
@@ -46,6 +59,7 @@ public class ProjectileItemAbility : ItemAbilityBase
 
     public override void End()
     {
+        character.animationEventReceiver.UnRegisterEvent(AnimationEventReceiver.EventEnum.ThrowBoom, Throw);
         base.End();
     }
 }
---
+++ Assets/Script/Item/ItemProjectile/RocketProjectile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class RocketProjectile : ItemProjectileBase
+{
+    private Vector3 initialVelocity = Vector3.zero;
+    public Quaternion itemRot = Quaternion.identity;
+    public Vector3 rocketForward = Vector3.zero;
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        rb.velocity = initialVelocity;
+    }
+
+    public override void Init(CharacterContorl character)
+    {
+        base.Init(character);
+    }
+
+    public override void Launch()
+    {
+
+    }
+
+    public override void Throw()
+    {
+        this.transform.parent = character.itemPlaceBelly;
+        this.transform.localPosition = Vector3.zero;
+        this.transform.rotation = itemRot;
+        this.transform.parent = null;
+        this.project = rocketForward;
+        Physics.IgnoreCollision(this.bodyCollider, character.bodyCollider, true);
+        rb.velocity = (character.ridbody.velocity.magnitude) * project;
+        initialVelocity = rb.velocity;
+    }
+}
+
