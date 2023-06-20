022abac04be6f2120ee5208fc0a3d76b64d1403d
Harry <liyawen117967@pwrd.com>
2023-06-20 10:37:35 +0800

    火箭功能

--- Assets/Script/Buff/ItemBuff/RocketThrusterBuff.cs
+++ Assets/Script/Buff/ItemBuff/RocketThrusterBuff.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using MudBun;
+using DG.Tweening.Core;
 
 public class RocketThrusterBuff : ItemBuffBase
 {
@@ -8,8 +11,10 @@ public class RocketThrusterBuff : ItemBuffBase
     private float runSpeedUpTime = 0.5f;
     //推进最大速度
     private float runMaxVelocity = 12f;
-
+    public ParticleSystem particle;
     GameObject rocket;
+    private Light light;
+    private MudNoiseVolume volume;
     public RocketThrusterBuff(CharacterContorl target) : base(target)
     {
         buffTime = 15f;
@@ -35,6 +40,21 @@ public class RocketThrusterBuff : ItemBuffBase
         rocket.transform.localPosition = new Vector3(0, 0, 0);
         rocket.gameObject.SetActive(true);
         rocket.transform.localPosition = new Vector3(0, 0, 0);
+        particle = rocket.GetComponentInChildren<ParticleSystem>();
+        light = rocket.GetComponentInChildren<Light>();
+        light.intensity = 0;
+        light.DOIntensity(1, 2);
+
+        volume = rocket.GetComponentInChildren<MudNoiseVolume>();
+        var getter = new DOGetter<float>(() =>
+        {
+            return volume.Threshold;
+        });
+        var setter = new DOSetter<float>((x) => {
+            volume.Threshold = x;
+        });
+        volume.Threshold = 1;
+        DOTween.To(getter, setter, 0.2f, 1);
     }
 
 
@@ -46,24 +66,28 @@ public class RocketThrusterBuff : ItemBuffBase
         character.anima.SetBool("inRocket", false);
 
         base.OnBuffRemove();
-        var point = (character.transform.position + (character.bodyCollider as SphereCollider).center) + (character.ridbody.transform.forward.normalized * (character.bodyCollider as SphereCollider).radius * character.transform.localScale.x + character.ridbody.transform.forward.normalized * 1);
-        //创建火箭
-        var itemBase = ItemManager.CreatProjectileByItemID(1, character, point);
-        (itemBase as RocketProjectile).itemRot = rocket.transform.rotation;
-        (itemBase as RocketProjectile).rocketForward = rocket.transform.forward;
-        itemBase.Throw();
-        rocket.gameObject.SetActive(false);
-        GameObject.Destroy(rocket);
+        GenerateRocket();
+        //rocket.gameObject.SetActive(false);
+        //GameObject.Destroy(rocket);
     }
 
     private void GenerateRocket()
     {
-
+        var point = (character.transform.position + (character.bodyCollider as SphereCollider).center) + (character.ridbody.transform.forward.normalized * (character.bodyCollider as SphereCollider).radius * character.transform.localScale.x + character.ridbody.transform.forward.normalized * 1);
+        //创建火箭
+        rocket.transform.parent = null;
+        var itemBase = rocket.AddComponent<RocketProjectile>();
+        itemBase.Init(character);
+        (itemBase as RocketProjectile).itemRot = rocket.transform.rotation;
+        Quaternion rotation = Quaternion.Euler(0f, -90f, 0f);
+        (itemBase as RocketProjectile).rocketForward = rotation * rocket.transform.forward;
+        itemBase.Throw();
     }
 
     public override void OnBuffUpdate()
     {
         base.OnBuffUpdate();
+
         if (character.isDead || character.isStun)
         {
             base.Finish();
