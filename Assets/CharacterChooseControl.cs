39478cbc72b6a7fc5b3d7335da98e67dc5bb3dee
Harry <liyawen117967@pwrd.com>
2023-06-25 11:22:02 +0800

    fur color

--- Assets/CharacterChooseControl.cs
+++ Assets/CharacterChooseControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XFurStudio2;
 
 public class CharacterChooseControl : MonoBehaviour
 {
@@ -11,14 +12,19 @@ public class CharacterChooseControl : MonoBehaviour
     public float H;
     public float S;
     private float originalS;
+    public FurData furData;
+    public int index;
+    public XFurStudioInstance xfurInstance;
     void Start()
     {
-        var rendererBlock = new MaterialPropertyBlock();
-        meshRenderer.GetPropertyBlock(rendererBlock, 1);
-        S = rendererBlock.GetFloat("_S");
-        H = rendererBlock.GetFloat("_H");
-        originalS = 0.51f;
-        SetColor();
+        //var rendererBlock = new MaterialPropertyBlock();
+        //meshRenderer.GetPropertyBlock(rendererBlock, 1);
+        //S = rendererBlock.GetFloat("_S");
+        //H = rendererBlock.GetFloat("_H");
+        //originalS = 0.51f;
+        index = 0;
+        //SetColor();
+        SetFurColor();
     }
 
 
@@ -37,28 +43,50 @@ public class CharacterChooseControl : MonoBehaviour
         meshRenderer.SetPropertyBlock(rendererBlock, 1);
     }
 
+    private void SetFurColor()
+    {
+        xfurInstance.FurDataProfiles[1].FurMainTint = furData.furDataList[index].furColor;
+    }
+
     public void SwitchColor(bool left)
     {
-        var x = left ? 0.1f : -0.1f;
-        if (S == 0)
+        //var x = left ? 0.1f : -0.1f;
+        //if (S == 0)
+        //{
+        //    S = originalS;
+        //    H += x;
+        //}
+        //else if (H + x > 1)
+        //{
+        //    S = 0;
+        //    H = 0;
+        //}
+        //else if (H + x < 0)
+        //{
+        //    H = 1;
+        //    S = 0;
+        //}
+        //else
+        //{
+        //    H += x;
+        //}
+        //SetColor();
+
+        var x = left ? -1 : 1;
+        var nextIndex = index + x;
+        if (nextIndex < 0)
         {
-            S = originalS;
-            H += x;
+            index = furData.furDataList.Count - 1;
         }
-        else if (H + x > 1)
+        else if (nextIndex > (furData.furDataList.Count - 1))
         {
-            S = 0;
-            H = 0;
-        }
-        else if (H + x < 0)
-        {
-            H = 1;
-            S = 0;
+            index = 0;
         }
         else
         {
-            H += x;
+            index = nextIndex;
         }
-        SetColor();
+
+        SetFurColor();
     }
 }
