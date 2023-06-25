8e93037c44da181a42ceb92df651019cc91a2550
Harry <liyawen117967@pwrd.com>
2023-06-25 17:53:25 +0800

    无敌判定

--- Assets/Script/CharacterContorl.cs
+++ Assets/Script/CharacterContorl.cs
@@ -1065,12 +1065,14 @@ public class CharacterContorl : MonoBehaviour
 
     public void AddForce(Vector3 force,ForceMode forceMode)
     {
-        ridbody.AddForce(force,forceMode);
+        if (!invulernable)
+            ridbody.AddForce(force,forceMode);
     }
 
     public void AddExplosionForce(float explosionForce,Vector3 explosionPositon,float explosionRadius)
     {
-        ridbody.AddExplosionForce(explosionForce, explosionPositon, explosionRadius);
+        if (!invulernable)
+            ridbody.AddExplosionForce(explosionForce, explosionPositon, explosionRadius);
     }
 
     private void UseItem(bool isUse)
