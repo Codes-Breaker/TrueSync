589210b5b6591978d86d6141bdbeb09a2859530c
linmohan <466343622@qq.com>
2023-06-27 17:51:14 +0800

    分屏补

---
+++ Assets/Script/Enum/GameMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum GameMode
+{
+    Single, //单人
+    Multiple, //多人
+}
+
+public enum ScreenMode
+{
+    Same,
+    Split
+}
